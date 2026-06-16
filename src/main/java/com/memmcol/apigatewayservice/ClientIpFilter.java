6cb7f5433368fab843dabadd0ba9be188cab97e7
H.Soliu <abeeb.ilias@gmail.com>
2026-06-16 09:03:02 +0100

    Client Ip forwarding done v9

--- src/main/java/com/memmcol/apigatewayservice/ClientIpFilter.java
+++ src/main/java/com/memmcol/apigatewayservice/ClientIpFilter.java
@@ -9,43 +9,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-
-//@Component
-//public class ClientIpFilter implements GlobalFilter {
-//
-//    private static final Logger log =
-//            LoggerFactory.getLogger(ClientIpFilter.class);
-//
-//    @Override
-//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-//
-//        var request = exchange.getRequest();
-//
-//        String clientIp = exchange.getRequest().getRemoteAddress() != null
-//                ? exchange.getRequest().getRemoteAddress().getAddress().getHostAddress()
-//                : "unknown";
-//
-////        String clientIp =  exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
-//
-//        String forwarded = request.getHeaders().getFirst("X-Forwarded-For");
-//
-//        log.info("Remote IP (TCP): {}", clientIp);
-//        log.info("X-Forwarded-For (incoming): {}", forwarded);
-//
-//        ServerHttpRequest mutatedRequest = exchange.getRequest()
-//                .mutate()
-//                .headers(h -> {
-//                    h.remove("X-Forwarded-For");
-//                    h.remove("X-Real-IP");
-//                    h.set("X-Client-IP", clientIp);
-//                })
-//                .build();
-//
-//        return chain.filter(exchange.mutate().request(mutatedRequest).build());
-//    }
-//}
-
-
 @Component
 public class ClientIpFilter implements GlobalFilter {
 
@@ -84,3 +47,40 @@ public class ClientIpFilter implements GlobalFilter {
         return chain.filter(exchange.mutate().request(mutatedRequest).build());
     }
 }
+
+
+
+//@Component
+//public class ClientIpFilter implements GlobalFilter {
+//
+//    private static final Logger log =
+//            LoggerFactory.getLogger(ClientIpFilter.class);
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//
+//        var request = exchange.getRequest();
+//
+//        String clientIp = exchange.getRequest().getRemoteAddress() != null
+//                ? exchange.getRequest().getRemoteAddress().getAddress().getHostAddress()
+//                : "unknown";
+//
+////        String clientIp =  exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
+//
+//        String forwarded = request.getHeaders().getFirst("X-Forwarded-For");
+//
+//        log.info("Remote IP (TCP): {}", clientIp);
+//        log.info("X-Forwarded-For (incoming): {}", forwarded);
+//
+//        ServerHttpRequest mutatedRequest = exchange.getRequest()
+//                .mutate()
+//                .headers(h -> {
+//                    h.remove("X-Forwarded-For");
+//                    h.remove("X-Real-IP");
+//                    h.set("X-Client-IP", clientIp);
+//                })
+//                .build();
+//
+//        return chain.filter(exchange.mutate().request(mutatedRequest).build());
+//    }
+//}
