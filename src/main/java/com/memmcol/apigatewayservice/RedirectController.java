3b566fdcb6aa7c054f7e9a18c076964617d30e27
H.Soliu <abeeb.ilias@gmail.com>
2026-06-16 13:16:53 +0100

    test ip

--- src/main/java/com/memmcol/apigatewayservice/RedirectController.java
+++ src/main/java/com/memmcol/apigatewayservice/RedirectController.java
@@ -1,28 +1,31 @@
-//package com.memmcol.apigatewayservice;
-//
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.server.reactive.ServerHttpRequest;
-//import org.springframework.http.server.reactive.ServerHttpResponse;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import reactor.core.publisher.Mono;
-//
-//import java.net.URI;
-//
-//@RestController
-//public class RedirectController {
-//
-//    @GetMapping("/**")
-//    public Mono<Void> redirect(ServerHttpRequest request, ServerHttpResponse response) {
-//
-//        URI uri = request.getURI();
-//
-//        String httpsUrl = "https://" + uri.getHost() + uri.getRawPath();
-//
-//        response.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
-//        response.getHeaders().setLocation(URI.create(httpsUrl));
-//
-//        return response.setComplete();
-//    }
-//}
+package com.memmcol.apigatewayservice;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@RestController
+public class RedirectController {
+
+    @GetMapping("/**")
+    public Mono<Void> redirect(ServerHttpRequest request, ServerHttpResponse response) {
+
+        URI uri = request.getURI();
+
+        System.out.println("=============:::1 "+uri);
+        System.out.println("=============:::2 "+request.getRemoteAddress());
+
+        String httpsUrl = "https://" + uri.getHost() + uri.getRawPath();
+
+        response.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        response.getHeaders().setLocation(URI.create(httpsUrl));
+
+        return response.setComplete();
+    }
+}
