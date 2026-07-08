0b8b0c9ed80690e39189001b6c17079a309d9480
H.Soliu <abeeb.ilias@gmail.com>
2026-07-08 13:20:23 +0100

    workflow modified

--- src/main/java/com/memmcol/apigatewayservice/ClientIpFilter.java
+++ src/main/java/com/memmcol/apigatewayservice/ClientIpFilter.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
